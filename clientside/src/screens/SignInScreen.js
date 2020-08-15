be536cb950e69c6043b7c01047ddf8895f0d73d3
atul kumar gautam <atulspl2019@gmail.com>
2020-08-15 14:58:26 +0530

    v5

--- clientside/src/screens/SignInScreen.js
+++ clientside/src/screens/SignInScreen.js
@@ -8,7 +8,7 @@ import Axios from 'axios';
 
 const SignScreens = (props) => {
    const [email,setEmail] = useState('')
-//    const [response,setResponse]=useState('')
+   const [response,setResponse]=useState('')
    const [password,setPassword] = useState('')
    const userSignin = useSelector(state=>state.userSignin)
    const {loading,userInfo,error} = userSignin
