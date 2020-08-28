01ef1f941a23b16f7ea46e14a22f24abe02cc57d
atul kumar gautam <atulspl2019@gmail.com>
2020-08-28 23:33:58 +0530

    paginate

--- serverside/config.js
+++ serverside/config.js
@@ -5,6 +5,9 @@ export const JWT_SECRET_KEY=process.env.JWT_SECRET_KEY||'@#$thisisaSecratekey';
     export const mongoURI= process.env.MONGODBURI_TEST;
     export const EMAIL=process.env.EMAIL;
     export const PASSWORD = process.env.PASSWORD
+    // console.log(process.env.accessKeyId,process.env.secretAccessKey)
+    export const accessKeyId = process.env.AWSAccessKeyId
+    export const secretAccessKey = process.env.AWSSecretKey
 
 export const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID_TEST //test id
 // export const PAYPAL_CLIENT_ID = process.env.PAYPAL_CLIENT_ID_PRODUCTION //live id
